bc4c6b37be198a88c60a371387b914d31745da9e
IMLHF <red_wind@foxmail.com>
2019-03-03 15:58:31 +0800

    # 233

--- fromServer.sh
+++
@@ -1,1 +0,0 @@
-scp -P 15041 student@speaker.is99kdf.xyz:~/work/dbn_test/test1/1* ./
--- toServer.sh
+++
@@ -1,2 +0,0 @@
-scp  -P 15041 ./*  student@speaker.is99kdf.xyz:~/work/dbn_test/test13/
-# scp -r -P 22 ./*  ssh lhf@hp.eligao.com:~/dbn_test/test13/
